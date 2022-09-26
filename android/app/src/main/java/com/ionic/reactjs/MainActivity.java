9d5d075cc5d05ddcb72e83f3856cc7a3ea99b413
Godwin Kimani <josephgodwinke@yandex.com>
2022-09-26 17:34:35 +0300

    works on web and android

---
+++ android/app/src/main/java/com/ionic/reactjs/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ionic.reactjs;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.reactjs',
+  appName: 'ionic-react-js',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
